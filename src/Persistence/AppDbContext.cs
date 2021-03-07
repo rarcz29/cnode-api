618db9d5f9a58dccf1678dc9faaa203e340675e4
rarcz29 <rafalcz29@gmail.com>
2021-03-07 15:06:19 +0100

    Create DbContext

---
+++ src/Persistence/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace CNode.Persistence
+{
+    class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(builder);
+        }
+    }
+}
