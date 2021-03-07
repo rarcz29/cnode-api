10e26905bc6f490b722bc9bc9a683ea9857b3ee7
rarcz29 <rafalcz29@gmail.com>
2021-03-07 15:50:24 +0100

    Standard configuration

---
+++ src/WebAPI/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace CNode.WebAPI
+{
+    internal static class DependencyInjection
+    {
+        public static IServiceCollection AddWebAPI(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CNode", Version = "v1" });
+            });
+            return services;
+        }
+    }
+}
--- src/WebAPI/Startup.cs
+++ src/WebAPI/Startup.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
 namespace CNode.WebAPI
 {
@@ -17,18 +16,13 @@ namespace CNode.WebAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddPersistence(Configuration);
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
-            });
+            services
+                .AddWebAPI()
+                .AddPersistence(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
