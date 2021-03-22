3cfe0e5b69b1ef5e4155c000f39c93df4e5487e9
rarcz29 <rafalcz29@gmail.com>
2021-03-22 16:13:11 +0100

    Small updates

--- src/WebAPI/Controllers/V1/AuthController.cs
+++ src/WebAPI/Controllers/V1/AuthController.cs
@@ -10,25 +10,20 @@ namespace CNode.WebAPI.Controllers.V1
     [Route("api/v1/[controller]")]
     public class AuthController : ControllerBase
     {
-        private readonly IUserManager _userManager;
         private readonly IMediator _mediator;
 
-        public AuthController(IUserManager userManager, IMediator mediator)
+        public AuthController(IMediator mediator)
         {
-            _userManager = userManager;
             _mediator = mediator;
         }
 
         [HttpGet("token")]
         public async Task<IActionResult> GetToken([FromQuery] GetTokenQuery query)
         {
-            var result = _mediator.Send(query);
-            return Ok(result);
-            //var token = _userManager.Authenticate(email, password);
-            //return token != null
-            //    // TODO: create dto's
-            //    ? Ok(new { Token = token, Type = "bearer" })
-            //    : Unauthorized(new { Message = "Wrong email or password"});
+            var result = await _mediator.Send(query);
+            return result?.Token != null
+                ? Ok(result)
+                : BadRequest();
         }
     }
 }
