52c3f774c59a030e0ce846edb6075989ad8d29f5
rarcz29 <rafalcz29@gmail.com>
2021-05-08 21:15:39 +0200

    Migrations: remove technology types

---
+++ src/Persistence/Migrations/20210508191419_RemoveTechnologyTypes.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace CNode.Persistence.Migrations
+{
+    public partial class RemoveTechnologyTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_technologies_technology_types_technology_type_id",
+                table: "technologies");
+
+            migrationBuilder.DropTable(
+                name: "technology_types");
+
+            migrationBuilder.DropIndex(
+                name: "ix_technologies_technology_type_id",
+                table: "technologies");
+
+            migrationBuilder.DropColumn(
+                name: "technology_type_id",
+                table: "technologies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "technology_type_id",
+                table: "technologies",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "technology_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    name_plural = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_technology_types", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_technologies_technology_type_id",
+                table: "technologies",
+                column: "technology_type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_technology_types_name",
+                table: "technology_types",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_technology_types_name_plural",
+                table: "technology_types",
+                column: "name_plural",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_technologies_technology_types_technology_type_id",
+                table: "technologies",
+                column: "technology_type_id",
+                principalTable: "technology_types",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
